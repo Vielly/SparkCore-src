67fcf62d60d4febeb436d9cde4a446585729095a
Vielly <1304022558@qq.com>
2020-08-22 11:46:37 +0800

    PruneDependency分析

--- core/src/main/scala/org/apache/spark/rdd/PartitionPruningRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/PartitionPruningRDD.scala
@@ -32,14 +32,31 @@ private[spark] class PartitionPruningRDDPartition(idx: Int, val parentSplit: Par
  * Represents a dependency between the PartitionPruningRDD and its parent. In this
  * case, the child RDD contains a subset of partitions of the parents'.
  */
+/*
+ *
+ * @param rdd 父RDD的实例引用
+ * @param partitionFilterFunc 是一个回调函数，作用是过滤出符合条件的父RDD的分区id集合
+ * @tparam T
+ */
 private[spark] class PruneDependency[T](rdd: RDD[T], partitionFilterFunc: Int => Boolean)
   extends NarrowDependency[T](rdd) {
 
+  /*
+   * 现根据父RDD的所有partition集合，根据父RDD的partition的index使用过滤函数过滤出符合条件的（父RDD）partition，
+   * 然后使用拉链操作将过滤出来的partition从 0 开始编号，最后使用父 partition （注意这里是Partition，不是分区id）和编号
+   * 实例化新的 PartitionPruningRDDPartition 实例，并放入 partitions 集合中，这里就相当于对父 RDD 的 partition 进行
+   * Filter剪枝操作。
+   */
   @transient
   val partitions: Array[Partition] = rdd.partitions
     .filter(s => partitionFilterFunc(s.index)).zipWithIndex
     .map { case(split, idx) => new PartitionPruningRDDPartition(idx, split) : Partition }
 
+  /*
+   * 现根据子 partition 的 id 获取到父 RDD 对应的分区，然后获取父 RDD 的 partition 的 index ，该 index 是父 RDD 对应此
+   * partition 的索引值，子RDD partition 和 父RDD partition的关系是 一对一的， 父RDD 和子RDD 的关系是 多对一，也可能是一对多，也可能是一对一。
+   * 简言之，在窄依赖中，子RDD 的partition 和 父RDD 的 partition 的关系是 一对一的。
+   */
   override def getParents(partitionId: Int): List[Int] = {
     List(partitions(partitionId).asInstanceOf[PartitionPruningRDDPartition].parentSplit.index)
   }
